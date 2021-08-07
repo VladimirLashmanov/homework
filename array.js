0a8562b2b48919e78a34b8ee97af08f1dc94b444
VladimirLashmanov <vov7las@gmail.com>
2021-08-07 15:31:16 +0300

    Initial commit

---
+++ array.js
@@ -0,0 +1,9 @@
+    let Items = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+document.write('<ul>');
+for (let item of Items){
+    document.write(`<li>${item}</li>`);
+}
+
+document.write('</ul>');
+
